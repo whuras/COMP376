8bf83a76c0794d8ab48a3c0ba6343081f35c3d90
Emanuel Sharma <send.emanuel@hotmail.com>
2020-10-31 16:55:11 -0400

    Conductor Script

---
+++ Assets/Scripts/Conductor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductor : MonoBehaviour
+{
+    public float SourceBPM;
+
+    AudioSource mMusicSource;
+    float mSourceBPS;
+    double mSourceStartTime;
+
+    void Start()
+    {
+        mMusicSource = GetComponent<AudioSource>();
+        mMusicSource.Play();
+        mSourceStartTime = AudioSettings.dspTime;
+        mSourceBPS = SourceBPM / 60f;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(GetTimeSinceBeat());
+        }
+    }
+    
+    public float GetTimeToBeat()
+    {
+        double scaledBeatTime = AudioSettings.dspTime * mSourceBPS;
+        int nearestBeat = (int) (scaledBeatTime + 0.5f);
+        return (float) (scaledBeatTime - nearestBeat);
+    }
+    
+    public float GetTimeSinceBeat()
+    {
+        double scaledBeatTime = AudioSettings.dspTime * mSourceBPS;
+        return (float) (scaledBeatTime - (int) scaledBeatTime);
+    }
+}
