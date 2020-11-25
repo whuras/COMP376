a51d0e4885f96df5bc04154a236c235366929c32
steveiaco <31911579+steveiaco@users.noreply.github.com>
2020-11-24 21:48:18 -0500

    Added enemy AI for ground enemies, hangman and animation in gallows, alongside hangman animations

---
+++ Assets/Scripts/Gallows.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gallows : MonoBehaviour
+{
+    private HealthController h;
+    // Start is called before the first frame update
+    void Start()
+    {
+        h = GetComponent<HealthController>();
+        h.OnDeath += GallowsEscape;
+    }
+
+    void GallowsEscape()
+    {
+        
+    }
+}
