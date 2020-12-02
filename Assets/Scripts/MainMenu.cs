e832fc11a315b9302dcb71f8ecd83f6b5dc05acb
Sean Heinrichs <Sean Heinrichs>
2020-12-01 21:38:23 -0500

    create start screen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject MainMenuButtons;
+    public GameObject OptionsScreen;
+
+    private AudioSource mSelectionSFX;
+
+    private void Start()
+    {
+        mSelectionSFX = GetComponent<AudioSource>();
+        OptionsScreen.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        mSelectionSFX.Play();
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenOptions()
+    {
+        mSelectionSFX.Play();
+        MainMenuButtons.SetActive(false);
+        OptionsScreen.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        mSelectionSFX.Play();
+        MainMenuButtons.SetActive(true);
+        OptionsScreen.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        mSelectionSFX.Play();
+        Application.Quit();
+    }
+}
