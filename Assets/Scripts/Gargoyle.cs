7386b87fe2fe8f5df2e06c29129916da4d95bed4
Emanuel Sharma <send.emanuel@hotmail.com>
2020-12-06 00:43:37 -0500

    Gargoyle

---
+++ Assets/Scripts/Gargoyle.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gargoyle : MonoBehaviour
+{
+    [Tooltip("Target to aim at")]
+    public Transform Target;
+    [Tooltip("Source of projectiles")]
+    public Transform MuzzlePosition;
+    [Tooltip("Health controller of enemy to attach actions to")]
+    public HealthController HealthController;
+    [Tooltip("Rendered object to dissolve when enemy is killed")]
+    public Dissolve Dissolve;
+    [Tooltip("Time taken to dissolve enemy on death")]
+    public float DissolveTime = 3f;
+    [Tooltip("Projectile to fire")]
+    public Projectile Projectile;
+    [Tooltip("Fire sound effects")]
+    public AudioClip FireSFX;
+    [Tooltip("Conductor object used to time actions")]
+    public Conductor Conductor;
+    [Tooltip("Audio Source to play sound effects from")]
+    public AudioSource AudioSource;
+    [Tooltip("Movement speed of enemy")]
+    public float MovementSpeed = 3f;
+
+    public Vector3 Destination
+    {
+        get => mDestination;
+        set
+        {
+            //transform.position = value;
+            mDestination = value;
+            mHasDestination = value != Vector3.zero;
+        }
+    }
+    Vector3 mDestination = Vector3.zero;
+    bool mHasDestination = false;
+
+    public int FireBeat = 0;
+    public int FireRate = 8;
+
+    bool CanFire = true;
+    bool HasNotFired = true;
+    float mTimeOfDeath = -10f;
+    float SwoopStart = -10f;
+    float SwoopTime = 2f;
+    float SwoopApex = -10f;
+    float SwoopDistance = 35f;
+    Vector3 SwoopDirection;
+    Vector3 SwoopStartPos;
+    float SwoopHeight;
+
+    Quaternion mRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthController.OnDeath += OnDeath;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Dissolve out if dead
+        float dissolveProgress = (Time.time - mTimeOfDeath) / DissolveTime;
+        if (dissolveProgress < 1 && dissolveProgress > 0)
+        {
+            Dissolve.SetDissolved(dissolveProgress);
+            return;
+        }
+
+        Move();
+        Fire();
+        Swoop();
+        transform.rotation = Quaternion.Lerp(transform.rotation, mRotation, Time.deltaTime * 5f);
+    }
+
+    /// <summary> Move if appropriate. </summary>
+    void Move()
+    {
+        // Return early if no destination.
+        if (mHasDestination == false)
+        {
+            return;
+        }
+
+        // Return early if destination is already reached.
+        Vector3 toDestination = Destination - transform.position;
+        if (toDestination.magnitude < 0.1f)
+        {
+            return;
+        }
+
+        // Head towards destination otherwise.
+        transform.position += toDestination.normalized * MovementSpeed * Time.deltaTime;
+        //SwoopStartPos += toDestination.normalized * MovementSpeed * Time.deltaTime;
+    }
+
+    /// <summary> Fire projectile if appropriate. </summary>
+    void Fire()
+    {
+        if (!CanFire)
+        {
+            return;
+        }
+        // Fire on appropriate beats
+        if (Conductor.GetBeat() % FireRate != FireBeat)
+        {
+            HasNotFired = true;
+            return;
+        }
+        else if (HasNotFired)
+        {
+            Projectile newProjectile = Instantiate(Projectile, MuzzlePosition.position, Quaternion.LookRotation(MuzzlePosition.forward));
+            newProjectile.Owner = HealthController.gameObject;
+            newProjectile.Damage = 5f;
+            AudioSource.PlayOneShot(FireSFX);
+            HasNotFired = false;
+        }
+    }
+
+    /// <summary> Swoop down. </summary>
+    void Swoop()
+    {
+        float x = (Time.time - SwoopStart) / SwoopTime;
+        if (x > 1)
+        {
+            CanFire = true;
+
+            // Turn to face target
+            Vector3 toTarget = Target.position - transform.position;
+            mRotation = Quaternion.LookRotation(toTarget);
+            return;
+        }
+
+        float denom = (x > SwoopApex) ? 1-SwoopApex : SwoopApex;
+        float y = Mathf.Pow((x - SwoopApex) / denom, 2);
+
+        Vector3 pos = SwoopDirection * x;
+        pos.y = -SwoopHeight * (1-y);
+
+        Vector3 newPosition = SwoopStartPos + pos;
+        Vector3 up = newPosition - transform.position;
+        Vector3 left = Vector3.Cross(up, Vector3.up);
+        Vector3 forward = Vector3.Cross(up, left);
+        transform.position = newPosition;
+        mRotation = Quaternion.LookRotation(forward, up);
+        Debug.DrawLine(transform.position, transform.position+up.normalized * 5f, Color.green);
+        Debug.DrawLine(transform.position, transform.position+left.normalized * 5f, Color.red);
+        Debug.DrawLine(transform.position, transform.position+forward.normalized * 5f, Color.blue);
+    }
+
+    public void StartSwoop(Vector3 target)
+    {
+        if (Time.time - SwoopStart < SwoopTime)
+        {
+            return;
+        }
+        Debug.Log("SwoopStart");
+        CanFire = false;
+        HasNotFired = false;
+        SwoopStart = Time.time;
+        SwoopDirection = target - transform.position;
+        SwoopHeight = -1.5f-SwoopDirection.y;
+        SwoopDirection.y = 0;
+        SwoopApex = SwoopDirection.magnitude / SwoopDistance;
+        SwoopDirection = SwoopDirection.normalized * SwoopDistance;
+        SwoopStartPos = transform.position;
+    }
+
+    void OnDeath()
+    {
+        mTimeOfDeath = Time.time;
+        Destroy(HealthController.gameObject);
+        Destroy(gameObject, DissolveTime);
+    }
+}
