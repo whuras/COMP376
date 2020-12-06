86f98c525d871ef18695b8dc0dec6f9ec0c0498e
Emanuel Sharma <send.emanuel@hotmail.com>
2020-12-06 00:41:58 -0500

    MeleeEnemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    public abstract Transform Target { get; set; }
+    public abstract HealthController HealthController { get; }
+}
