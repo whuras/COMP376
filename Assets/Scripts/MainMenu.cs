82f46af7fe1e172bb3ae6fb35e2cf0c6afcc46d1
steveiaco <31911579+steveiaco@users.noreply.github.com>
2020-12-11 20:32:27 -0500

    Cleaned up sound files for tutorial and level 2 (#80)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -15,6 +15,8 @@ public class MainMenu : MonoBehaviour
     {
         mSelectionSFX = GetComponent<AudioSource>();
         OptionsScreen.SetActive(false);
+        // Destroy it just in case we came from a restart
+        Destroy(Conductor.GetActiveConductorGameObject());
         mConductor = Conductor.GetActiveConductor();
         Cursor.lockState = CursorLockMode.None;
     }
